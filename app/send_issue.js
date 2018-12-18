a1a7eb84cf5b919828742b0796eb793063ea08ef
JordanVScher <jordanvscher@hotmail.com>
2018-12-18 12:30:43 -0200

    Dialogflow update

--- app/send_issue.js
+++ app/send_issue.js
@@ -1,5 +1,6 @@
 const accents = require('remove-accents');
-const MandatoAbertoAPI = require('./mandatoaberto_api.js');
+const MaAPI = require('./mandatoaberto_api');
+const { issueText } = require('./utils/flow');
 
 const blacklist = ['sim', 'nao'];
 
@@ -12,22 +13,20 @@ async function formatString(text) {
 }
 module.exports.formatString = formatString;
 
-
 // check if we should create an issue with that text message.If it returns true, we send the appropriate message.
-async function createIssue(context, textToSend) {
-	if (!textToSend || textToSend.length === 0) {
-		textToSend = 'Não entendi sua mensagem. Você pode escrever novamente?';
-	}
+async function createIssue(context) {
 	// check if text is not empty and not on the blacklist
 	const cleanString = await formatString(context.state.whatWasTyped);
-	if (cleanString && cleanString.length > 1 && !blacklist.includes(cleanString)) {
-		if (await MandatoAbertoAPI.postIssue(context.state.politicianData.user_id, context.session.user.id,
-			context.state.whatWasTyped, context.state.resultParameters, context.state.politicianData.issue_active) !== false) {
+	if (cleanString && cleanString.length > 0 && !blacklist.includes(cleanString)) {
+		const issueResponse = await MaAPI.postIssue(context.state.politicianData.user_id, context.session.user.id, context.state.whatWasTyped,
+			context.state.resultParameters ? context.state.resultParameters : {}, context.state.politicianData.issue_active);
+
+		if (issueResponse && issueResponse.id) {
+			await context.sendText(issueText.success);
 			return true;
 		}
 	}
-	//await context.sendText(textToSend);
+	await context.sendText(issueText.failure);
 	return false;
 }
-
 module.exports.createIssue = createIssue;
---
+++ app/utils/flow.js
@@ -0,0 +1,14 @@
+module.exports = {
+	avatarImage: 'https://www.telegraph.co.uk/content/dam/news/2016/09/08/107667228_beech-tree-NEWS_trans_NvBQzQNjv4BqplGOf-dgG3z4gg9owgQTXEmhb5tXCQRHAvHRWfzHzHk.jpg?imwidth=450',
+	greetings: {
+		text1: 'Hi, im a chatbot',
+		text2: 'I do this and that',
+	},
+	mainMenu: {
+		text1: 'on mainMenu',
+	},
+	issueText: {
+		success: 'Thank you for your message. We will answer you soon',
+		failure: 'I failed at saving the message',
+	},
+};
