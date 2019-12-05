0269cccdba6c0939078c1888bb073bf04edea9a6
JordanVScher <jordanvscher@hotmail.com>
2019-12-05 18:47:41 -0300

    Update soma_broadcast

--- app/soma_broadcast.js
+++ app/soma_broadcast.js
@@ -4,28 +4,36 @@ const fs = require('fs');
 const broadcast = require('./utils/broadcast');
 
 const testFolder = './.sessions/';
+const integerRegex = /^-?[0-9]+$/;
 
-async function sendMultipleMessages(users, text, res) {
+async function sendMultipleMessages(users, text, res, notFound) {
 	const results = {};
-	results.alunos_in_turma = users.length;
+	results.users_found = users.length;
 	results.successes = 0;
 	results.failures = 0;
 	results.details = [];
 	for (let i = 0; i < users.length; i++) {
 		const e = users[i];
-		const aux = await broadcast.sendBroadcastAluna(e.id, text);
-		if (aux && aux.message_id) {
-			results.successes += 1;
-      results.details.push({ aluno_name: e.name, message_id: aux.message_id, recipient_id: e.id, status: 'success',  }); // eslint-disable-line
-		} else {
+		if (!integerRegex.test(e.id) || e.id.toString().length !== 16) {
 			results.failures += 1;
-			results.details.push({ aluno_name: e.name, recipient_id: e.id, status: 'failure' });
+			results.details.push({ aluno_name: e.name, recipient_id: e.id, error: 'Invalid user key. User fb_id must be integer with 16 digits.', status: 'failure' }); // eslint-disable-line object-curly-newline
+		} else {
+			const aux = await broadcast.sendBroadcastAluna(e.id, text);
+			if (aux && aux.message_id) {
+				results.successes += 1;
+				results.details.push({ aluno_name: e.name, message_id: aux.message_id, recipient_id: e.id, status: 'success',  }); // eslint-disable-line
+			} else {
+				results.failures += 1;
+				results.details.push({ aluno_name: e.name, recipient_id: e.id, status: 'failure' });
+			}
 		}
 	}
+	if (notFound) results.users_not_found = notFound.length;
+	if (notFound) results.details_not_found = notFound;
 	res.status(200); res.send(results);
 }
 
-async function sendResponse(result, res) {
+async function sendResponse(result, res) { // eslint-disable-line
 	if (result && result.id) { res.status(200); res.send({ response: 'success', broadcast_id: result.id }); }
 	if (result && result.message_id) { res.status(200); res.send({ response: 'success', message_id: result.message_id, recipient_id: result.recipient_id }); }
 	res.status(500); res.send({ error: 'Couldnt send broadcast' });
@@ -41,7 +49,8 @@ async function getUsers() {
 
 	return result;
 }
-async function findUserByFBID(FBID) {
+
+async function findUserByFBID(FBID) { // eslint-disable-line
 	let res = false;
 	await fs.readdirSync(testFolder).forEach(async (file) => {
 		const obj = JSON.parse(await fs.readFileSync(testFolder + file, 'utf8'));
@@ -52,7 +61,19 @@ async function findUserByFBID(FBID) {
 
 	return res;
 }
-async function findUserByState(value, key) {
+async function findUserByFBIDList(users) { // eslint-disable-line
+	const res = [];
+	await fs.readdirSync(testFolder).forEach(async (file) => {
+		const obj = JSON.parse(await fs.readFileSync(testFolder + file, 'utf8'));
+		if (users.includes(obj.user.id)) {
+			res.push({ id: obj.user.id, name: obj.user.name });
+		}
+	});
+
+	return res;
+}
+
+async function findUserByState(value, key) { // eslint-disable-line
 	const res = [];
 	await fs.readdirSync(testFolder).forEach(async (file) => {
 		const obj = JSON.parse(await fs.readFileSync(testFolder + file, 'utf8'));
@@ -64,43 +85,29 @@ async function findUserByState(value, key) {
 	return res;
 }
 
-const integerRegex = /^-?[0-9]+$/;
 
 async function handler(res, body) {
 	if (!body) { res.status(400); res.send({ error: 'Empty body' }); }
 	if (!body.token_api) { res.status(400); res.send({ error: 'Param token_api missing' }); }
 	if (body.token_api !== process.env.API_TOKEN) { res.status(400); res.send({ error: 'Invalid token_api' }); }
 	const { text } = body;
-	const { turma } = body;
-	const { user } = body;
+	const { users } = body;
 
 	if (!text) { res.status(400); res.send({ error: 'Param text missing' }); }
-	if (turma && user) { res.status(400); res.send({ error: "Dont send both 'turma' and 'user' keys. Choose one or none to send for all users." }); }
-	if (!turma && !user) {
-		// const result = await broadcast.broadcastAll(text);
-		// await sendResponse(result, res);
-		const users = await getUsers();
-		await sendMultipleMessages(users, text, res);
+	if (Array.isArray(users) !== true) { res.status(400); res.send({ error: 'Invalid users format.' }); }
+
+	if (!users) {
+		const usersToSend = await getUsers();
+		await sendMultipleMessages(usersToSend, text, res);
 	}
 
-	if (user) {
-		if (!integerRegex.test(user) || user.toString().length !== 16) {
-			res.status(400); res.send({ error: 'Invalid user key. User fb_id must be integer with 16 digits.' });
-		} else if (await findUserByFBID(user) !== true) {
-			res.status(400); res.send({ error: `User with fb_id '${user}' not found!` });
-		} else {
-			const result = await broadcast.sendBroadcastAluna(user, text);
-			await sendResponse(result, res);
-		}
-	}
 
-	// const { escola } = body;
-	// if (turma && !escola) { res.status(400); res.send({ error: "Missing 'escola' param. Always send 'escola' together with 'turma'." }); }
-	// if (!turma && escola) { res.status(400); res.send({ error: "Missing 'turma' param. Always send 'escola' together with 'turma'." }); }
-	if (turma) {
-		const users = await findUserByState(turma, 'userTurmaID');
-		if (!users || users.length === 0) { res.status(400); res.send({ error: `No aluno in 'turma' ${turma}` }); }
-		await sendMultipleMessages(users, text, res);
+	if (users) {
+		const usersToSend = await findUserByFBIDList(users.map(String));
+		let notFound;
+		if (usersToSend.length !== users.length) { notFound = await users.filter(x => !usersToSend.find(y => y.id === x)); }
+
+		await sendMultipleMessages(usersToSend, text, res, notFound);
 	}
 }
 
