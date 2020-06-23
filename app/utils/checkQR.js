a7a3019971eca6b1c6eac9d91811d2790f436fd7
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-06-23 16:53:16 -0300

    Add Ver Produtos

--- app/utils/checkQR.js
+++ app/utils/checkQR.js
@@ -4,9 +4,11 @@ async function buildMainMenu(context) {
 	const tokenSMS = { content_type: 'text', title: 'Entrar SMS', payload: 'activateSMS' };
 	const meusPontos = { content_type: 'text', title: 'Meus Pontos', payload: 'myPoints' };
 	const escolaPontos = { content_type: 'text', title: 'Minha Escola', payload: 'schoolPoints' };
+	const verProdutos = { content_type: 'text', title: 'Ver Produtos', payload: 'viewAllProducts' };
 
 	if (context.state.somaUser && context.state.somaUser.id) {
 		res.push(meusPontos);
+		res.push(verProdutos);
 
 		const { schoolData } = context.state;
 		if (schoolData && schoolData.balance) res.push(escolaPontos);
