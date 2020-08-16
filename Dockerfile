47dbacd7084368c73c92b024f3d2c2f8d3cbf73c
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-08-16 17:28:13 -0300

    aaa

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,6 @@ RUN npm install && npm cache clean --force
 
 COPY . .
 
-ARG EXPORT_PORT
-
-EXPOSE $EXPORT_PORT
-
 RUN npx sequelize-cli db:migrate
 
 CMD ["npm", "run" ,"start"]
--- sample-run-container.sh
+++ sample-run-container.sh
@@ -7,18 +7,14 @@ if [ -f .env ]; then
     # For instance, will be example_kaggle_key
 fi
 
-echo $PAGE
-
 # arquivo de exemplo para iniciar o container
-export SOURCE_DIR='/home/jordan/soma-chatbot-homol'
-export DATA_DIR='/tmp/soma-chatbot-homol/data/'
+export SOURCE_DIR="${PWD}"
+export DATA_DIR="/tmp/${CONTAINER_NAME}/data/"
 
 # confira o seu ip usando ifconfig docker0|grep 'inet addr:'
 export DOCKER_LAN_IP=$(ifconfig docker0 | grep 'inet addr:' | awk '{ split($2,a,":"); print a[2] }')
 
-# porta que sera feito o bind
-
-
+# porta que será feito o bind
 docker run --name $CONTAINER_NAME \
  -v $SOURCE_DIR:/src -v $DATA_DR:/data \
  -p $DOCKER_LAN_IP:$PORT:$PORT \
